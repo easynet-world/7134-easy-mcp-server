790775a19037a17a3a5044229b7c4394eb3c491d
Boqiang Liang <boqiang@openai.com>
2025-08-23 14:21:34 -0700

    refactor: move sendSuccess and sendError to BaseProcessor
    - Refactor HelloProcessor to extend BaseProcessor
    - Remove duplicate sendSuccess and sendError methods
    - Add .releaserc configuration file
    - Update package.json dependencies and remove release config
    - Clean up code structure for better maintainability

--- api/hello/get.js
+++ api/hello/get.js
@@ -1,26 +1,8 @@
-class HelloProcessor {
-  constructor() {
-    this.name = this.constructor.name;
-  }
+const BaseProcessor = require('../src/BaseProcessor');
 
+class HelloProcessor extends BaseProcessor {
   process(req, res) {
     this.sendSuccess(res, { message: 'Hello World!' });
-  }
-
-  sendSuccess(res, data, statusCode = 200) {
-    res.status(statusCode).json({
-      success: true,
-      data,
-      timestamp: new Date().toISOString()
-    });
-  }
-
-  sendError(res, message, statusCode = 400) {
-    res.status(statusCode).json({
-      success: false,
-      error: message,
-      timestamp: new Date().toISOString()
-    });
   }
 
   get openApi() {
